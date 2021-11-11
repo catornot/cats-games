ee01898c5de150cb9b45e69b94977c64bc07087d
cat_or_not <41955154+catornot@users.noreply.github.com>
2021-11-10 19:10:17 -0500

    testing phase

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
-import time
-import importlib
-import sys
+from pywebio.output import *
+from pywebio.input import *
+from pywebio.session import set_env
+from pywebio import start_server
+from pywebio.output import output as output
 
-with open("test.py","r") as code:
-    content = code.read()
-
-with open("test.py","w") as code:
-    code.write("")
-
-try:
-    import test
-except:
-    print("Import error")
-    sys.exit()
-
-with open("test.py","w") as code:
-    code.write(content)
+import PIL
+import asyncio
 
 
+class Client:
+    def __init__(self):
+        self._run = True
+        self.screen = 0
+        self.counter = 0
 
-start = time.time()
-error = False
+    def callback(self):
+        self.var.reset()
+        self.counter += 1
+        self.var.append("click " + str(self.counter))
 
-while time.time() < start + 30:
+    def mainMenu(self):
+        self.var = output("")
+        put_scrollable(self.var)
+        put_row([
+            put_image(PIL.Image.open("24.png"), format="png", title='', width="100", height="100").onclick(self.callback).style("outline: 4px solid #e73;outline-offset: 4px;background: #ffa"),
+            put_button("label", onclick=self.callback, color="primary",
+                       small=False, link_style=False, outline=True),
+            put_button('ok2', onclick=self.callback),
+            put_button('ok1', onclick=self.callback),
+        ], size=50)
+    def run(self):
+        set_env(title='clicking')
+        if self.screen == 0:
+            self.mainMenu()
 
-    try:
-        importlib.reload(test)
-    except Exception as e:
-        if hasattr(e, 'message'):
-            print(e.message)
-        else:
-            print(e)
 
-    try:
-        import test
-        test.Test(test = "huh")
+client=Client()
 
-
-    except Exception as e:
-        if hasattr(e, 'message'):
-            print(e.message)
-        else:
-            print(e)
+if __name__ == '__main__':
+    start_server(client.run, port=80)
--- test.py
+++
@@ -1,3 +0,0 @@
-def Test(**uhw):
-    print(uhw["test"])
-    

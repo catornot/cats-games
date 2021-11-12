9eabd9d826cfacc25c9bed4e1bc9a8717be86dbe
cat_or_not <41955154+catornot@users.noreply.github.com>
2021-11-11 21:03:28 -0500

    added a login system
    idk if it works no sign up system yet lol

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pywebio.output import output as output
 import PIL
 import asyncio
 import sys
+import pickle
 
 try:
     print(sys.argv[1])
@@ -20,7 +21,36 @@ except:
 class Account_Manager:
     def __init__(self):
         pass
-    def login_validate(self):
+    def login_validate(self,data):
+        with open("info.txt","rb") as info:
+            infomation = pickle.load(info)
+        if data["nickname"] in infomation.keys():
+            if not infomation[data["nickname"]][1] == data["password"]:
+                return ("password","wrong password")
+            else:
+                clear()
+                start_main_menu(nickname=data["nickname"])
+        else:
+            return ("nickname","wrong nickname")
+
+    def signup_validate(self,nickname,email,password):
+        with open("info.txt","rb") as info:
+            infomation = pickle.load(info)
+            infomation[nickname] = [email,password,nickname]
+        with open("info.txt","wb") as info:
+            pickle.dump(infomation,info)
+
+class login_Screen:
+    def to_signup(self):
+        clear()
+        self.run_signup()
+    def run_login(self):
+        put_markdown("No account?, Sign up!").onclick(self.to_signup)
+        data = input_group("Sign in",[
+            input('Input your nickname', name='nickname'),
+            input('Input your password', name='password', type=PASSWORD)
+        ], validate=account_manager.login_validate,cancelable=False)
+    def run_signup(self):
         pass
 
 class Select_24_game:
@@ -44,11 +74,17 @@ class Select_24_game:
 
 class Main_Menu:
     def to_24(self):
-        clear(scope=None)
+        clear()
         start_select_24_game()
+    def to_signin(self):
+        clear()
+        start_signin()
 
     def run(self):
-        put_image(PIL.Image.open("cover.png"), format="png")
+        put_row([
+        put_image(PIL.Image.open("cover.png"), format="png"),
+        put_button('sign in', onclick=self.to_signin)
+        ], size=10)
         put_row([
             put_image(PIL.Image.open("24.png"), format="png", title='24 games', width="100", height="100").onclick(self.to_24).style("outline: 4px solid #e73;outline-offset: 4px;background: #ffa"),
         ], size=10)
@@ -57,15 +93,20 @@ class Main_Menu:
             put_text("24 games"),
         ], size=10)
 
+account_manager = Account_Manager()
 
-def start_main_menu():
+def start_main_menu(nickname="guest"):
     set_env(title='clicking')
     main_menu=Main_Menu()
     main_menu.run()
 
-def start_select_24_game():
+def start_select_24_game(nickname="guest"):
     select_24_game=Select_24_game()
     select_24_game.run()
+def start_signin():
+    login_screen=login_Screen()
+    login_screen.run_login()
+
 
 if __name__ == '__main__':
     start_server(start_main_menu, port=port)
